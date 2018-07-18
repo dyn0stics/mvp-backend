fec64abe97a01644ba32b87a91ab9e44c7a93421
Ivan Reif <ivanreif@gmail.com>
2018-07-18 10:47:00 +0200

    cors

--- src/main/java/io/dyno/mvp/Application.java
+++ src/main/java/io/dyno/mvp/Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.web3j.abi.datatypes.generated.Bytes16;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
@@ -21,10 +23,14 @@ import java.util.List;
 @PropertySource("application.properties")
 @EnableWebMvc
 @ComponentScan
-public class Application {
+public class Application implements WebMvcConfigurer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
